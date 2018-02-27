bf0ba17370fcc2004afc7e052670cff729b4ff02
Alok Bhave <alok_bhave@yahoo.com>
2018-02-27 23:48:38 +0530

    Update DbBrowser.php

--- DbBrowser.php
+++ DbBrowser.php
@@ -9,10 +9,10 @@
   $link = mysqli_connect('mysql:3306','alokbhav_abbhave','alokb123') or die('Could no connect: ' . mysql_error());
   echo "connected to db\n";
   $query = 'show databases' ;
-  $db = mysqli_query($query) or die('Query failed: ' . mysql_error());
+  $db = mysqli_query($link,$query) or die('Query failed: ' . mysql_error());
   echo "Query successful\n";
   echo "<table>\n";
-  while ($line = mysqli_fetch_array($db, MYSQL_ASSOC)) {
+  while ($line = mysqli_fetch_array($db, MYSQLI_ASSOC)) {
 	  echo "<tr>\n";
 	  foreach ($line as $colval)
 	    echo "\t<td><a href=ShowTables.php?dbname=$colval>$colval</a></td>\n";
