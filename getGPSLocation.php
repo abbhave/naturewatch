88f56f5050a5f88b996a51353781ee3e21d667f0
Alok Bhave <alok_bhave@yahoo.com>
2018-03-02 14:08:40 +0530

    Update getGPSLocation.php

--- getGPSLocation.php
+++ getGPSLocation.php
@@ -1,16 +1,16 @@
 <?php
 	error_reporting(E_ALL ^ E_DEPRECATED);
-	$link = mysql_connect('localhost', 'u802345539_alok', 'alokb123')
+	$link = mysqli_connect('mysql:3306','alokbhav_abbhave','alokb123')
     	or die('Could not connect: ' . mysql_error());
 
-	mysql_select_db('u802345539_alok') or die('Could not select database');
+	mysqli_select_db('alokbhav_abbhave') or die('Could not select database');
 	
 	$query="select LocationName,GPSLocation,LocationDescription from locationinfo";
 	
 	$jsonresp = "{\"locations\":[";
 	$count=0;
-	$queryResult = mysql_query($query) or die('Query failed: ' . mysql_error());
-	while($locationlist = mysql_fetch_array($queryResult, MYSQL_BOTH)){  
+	$queryResult = mysqli_query($link,$query) or die('Query failed: ' . mysql_error());
+	while($locationlist = mysqli_fetch_array($queryResult, MYSQLI_BOTH)){  
 			if($count != 0) $jsonresp.=",";
 			$locationname=$locationlist['LocationName'];
 			$gpslocation=$locationlist['GPSLocation'];
@@ -19,9 +19,9 @@
 			$count=1;
 			//$querycateg="select category from ImageInfo where 'LocationName'='".$locationlist['LocationName']."'";
 			$querycateg="select DISTINCT(Category) from ImageInfo where LocationName='".$locationname."'";
-			$queryCategResult = mysql_query($querycateg) or die('Query failed: ' . mysql_error())."'";
+			$queryCategResult = mysqli_query($link,$querycateg) or die('Query failed: ' . mysql_error())."'";
 			$countcateg=0;
-			while($categorylist = mysql_fetch_array($queryCategResult, MYSQL_BOTH)){
+			while($categorylist = mysqli_fetch_array($queryCategResult, MYSQLI_BOTH)){
 				if($countcateg != 0) $jsonresp.=",";
 				$jsonresp.="\"".$categorylist['Category']."\"";
 				$countcateg=1;
