245c370ddf250163a6bcd1cc41aa225abc21ce79
Alok Bhave <alok_bhave@yahoo.com>
2018-03-11 17:41:44 +0530

    Update ShowTables.php

--- ShowTables.php
+++ ShowTables.php
@@ -6,14 +6,14 @@
 </head>
 <body>
 <?php
-  $link = mysql_connect('localhost','alokbhav_abbhave','alokb123') or die('Could no connect: ' . mysql_error());
-  $db = mysql_select_db($_GET['dbname']) or die('Could not connect to db: ' . mysql_error());
+  $link = mysqli_connect('localhost','alokbhav_abbhave','alokb123') or die('Could no connect: ' . mysql_error());
+  $db = mysqli_select_db($_GET['dbname']) or die('Could not connect to db: ' . mysqli_error());
   $dbname = $_GET['dbname'];
   $query = 'show tables' ;
-  $result = mysql_query($query) or die('Query failed: ' . mysql_error());
+  $result = mysqli_query($query) or die('Query failed: ' . mysqli_error());
   echo "Query successful\n";
   echo "<table>\n";
-  while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+  while ($line = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
 	  echo "<tr>\n";
 	  foreach ($line as $colval)
 	    echo "\t<td><a href=ShowContents.php?dbname=$dbname&tablename=$colval>$colval</a></td>\n";
